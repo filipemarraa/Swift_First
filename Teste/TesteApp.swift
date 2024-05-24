7dbb8c2901552a85840a329c72d2eaeca49f54e5
Filipe Jacobson Marra <filipejacobsonmarra@gmail.com>
2024-05-24 09:12:15 -0300

    Initial Commit

---
+++ Teste/TesteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesteApp.swift
+//  Teste
+//
+//  Created by Filipe Jacobson Marra on 24/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TesteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
